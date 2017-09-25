2805c124caa25b5b68ac0eb9b5986ebfcc4b1dff
rmoayinla <rmoayinla@yahoo.com>
2017-09-25 18:01:56 +0100

    Told you this class is a mess, now looking a little bit cleaner, still have some work to do though. I want this class to implement a Renderer interface

--- display-fields.php
+++ display-fields.php
@@ -23,10 +23,22 @@ class Abbey_Profile_Field {
 	private $options = array();
 
 	/**
-	 * Json data container datas that will be used to populate some fields dynamically 
+	 * Json data container,  datas that will be used to populate some fields dynamically 
 	 *@var: array
 	 */
 	private $data_json = array();
+
+	private $field_id = '';
+
+	private $field_type = 'text';
+
+	private $field_class = [];
+
+	private $field_attributes = '';
+
+	private $field_name = '';
+
+	private $field_value = '';
 	
 	/**
 	 * Constructor 
@@ -39,13 +51,12 @@ class Abbey_Profile_Field {
 	}
 
 	public function display_field( $args ){
-		$field = $value = $name = $id = "";
-		$value = "";
+		$field = $value = $name = $id = $attributes = $class ="";
 		$name = $args[ "name" ]; 
 		$id = $args[ "id" ];
 		$field = "<fieldset>";
 		$args[ "attributes" ][ "data-name" ] = $name;
-		$attributes = $class = ""; 
+	
 		
 		
 		if( empty( $args[ "repeater_key" ] ) && empty( $args[ "repeater_no" ] )  ){
@@ -101,7 +112,7 @@ class Abbey_Profile_Field {
 								$attributes
 							 ); 
 			$respond_data = array();
-			$choices = !empty( $args[ "choices" ]) ? $args[ "choices" ] : array();
+			$choices = !empty( $args[ "choices" ] ) ? $args[ "choices" ] : array();
 
 			if( !empty( $args[ "attributes" ][ "data-json" ] )  ){
 				
@@ -201,20 +212,117 @@ class Abbey_Profile_Field {
 		echo $field; print_r( $args );;
 	}
 
+	private function create_attributes( $args ){
+		//set the id attribute of the field //
+		if( !empty( $args[ "id" ] ) $this->field_id = $args[ "id" ];
+		
+		//set the name attribute //
+		if( !empty( $args[ "name" ] ) $this->field_name = $args[ "name" ];
+		
+		//set the data-name attributes, used in JS //
+		$args[ "attributes" ][ "data-name" ] = $this->field_name;
+		
+		//add profile-$key to class attributes for fields //
+		if( !empty( $args[ "key" ] ) ) $this->field_class[] = "profile-".esc_attr( $args[ "key" ] );
+
+		//if we are not in a repeater, get the value attribute from our options  //
+		if( !$this->is_repeater( $args ) ){
+			$this->field_value = $this->get_field_value( $args[ "section_key" ], $args[ "key" ], $this->options );
+		}
+		//we are on a repeater field //
+		elseif( !empty( $args[ "repeater_key" ] ) ) {
+			if( !empty ( $this->options[ "repeater" ][ $args[ "repeater_key" ] ] ) ){
+				$repeater = $this->options[ "repeater" ][ $args[ "repeater_key" ] ];
+				if( is_array( $repeater ) && array_key_exists( $args[ "repeater_no" ], $repeater ) )
+					$this->field_value = $this->get_field_value( $args[ "repeater_no" ], $args[ "key" ], $repeater );
+			}
+
+			$args[ "attributes" ][ "data-repeater" ] = $args[ "repeater_no" ];
+			//add a repeater class to our field class attributes //
+			$this->field_class[] = "repeater-group";
+		}
+
+		//no attributes, we are done just bail //
+		if( empty( $args[ "attributes" ] ) ) return;
+
+		/**
+		 * IF we have classes set in our $args['attributes'], loop and add them to our field class
+		 * then remove the class index from $args['attributes']
+		 */
+		if( !empty( $args[ "attributes" ][ "class" ] ) ){
+			foreach ( $args[ "attributes" ][ "class" ] as $class ){
+				$this->field_class[] = $class;	
+			}
+			unset( $args[ "attributes" ][ "class" ] );
+		}	
+
+		//loop and add our remaining attributes e.g. col, row, data-name etc //
+		foreach( $args[ "attributes" ] as $attribute => $attr_value ){
+			if( is_array( $attr_value ) )
+				$this->field_attributes .= sprintf('%1$s="%2$s"', $attribute, esc_attr( implode( $attr_value, " " ) ) );
+			else
+				$this->field_attributes .= sprintf('%1$s="%2$s"', $attribute, esc_attr( $attr_value ) );
+		}
+		
+
+	}
+
+	/**
+	 * Is the current field a repeater field
+	 * all repeater fields must have a 'repeater_no' or 'repeater_key' in their $args array 
+	 *@return: 	bool 		true|false 
+	 *@param: $args 		array 		field info from WP Settings API 
+	 */
+	private function is_repeater( $args ){
+		if( !empty( $args[ "repeater_key" ] ) || !empty( $args[ "repeater_no" ] ) ) return true;
+		
+		return false;
+	}
 	/**
 	 * Populate value of fields that already have some existing data 
 	 * fields that have values previously stored are prefilled so that they can be updated
+	 *@param: 	$section_key 	string 		the key to search for in fields container 
+	 *			$key 			string 		the key to search for in the section_key in fields 
+	 *			$options 		array 		user saved profile data 
 	 *@return: 	mixed 		depending on if the value is found 
 	 */
 	private function get_field_value( $section_key, $key = "", $options ){
-		if( !array_key_exists( $section_key, $options ) ){
-			if( in_array( $section_key, $options ) )
-		}
-			$section = $options[ $section_key ];
-			if( is_array( $section ) )
-				return !empty( $section[ $key ] ) ? $section[ $key ] : var_export( $section );
-		}
+		
+		//copy the option if the key exist in our $options data //
+		if( array_key_exists( $section_key, $options ) ) $section = $options[ $section_key ];
+
+		//ok, the key doesnt exist but we have a value, still copy it //
+		elseif( $key = array_search( $section_key, $options ) ) $section = $options[ $key ];
+		
+		if( empty( $section ) ) return; //empty, simply bail //
+
+		if( is_scalar( $section ) ) return $section; //not an array, bail again //
+		
+		if( is_array( $section ) )
+			return !empty( $section[ $key ] ) ? $section[ $key ] : var_export( $section, false );
+		
 		return "";
+	}
+
+	function input_field(){
+		 $field .= sprintf( '<input type="%1$s" name="%2$s" value="%3$s" id="%4$s" class="%5$s" %6$s />', 
+						esc_attr( $args["type"] ), 
+						esc_attr( $name ), 
+						$value, 
+						esc_attr( $id ), 
+						esc_attr( implode( $class, " " ) ),
+						$attributes
+
+					);
+	}
+
+	function select_field(){
+
+	}
+
+	function textarea_field(){
+
+	}
 	
 
 	
