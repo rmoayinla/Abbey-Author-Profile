5bdd529a6c399724e6f2e8132bb13f28bdb4c5fc
rmoayinla <rmoayinla@yahoo.com>
2017-10-03 17:19:43 +0100

    A render interface, not implemented yet

---
+++ includes/Renderer.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Interface for rendering/displaying datas 
+ *
+ * Data can be rendered in HTML, Form, PDF, etc 
+ *
+ */
+interface Renderer{
+	
+	public function render(){
+		
+	}
+}
